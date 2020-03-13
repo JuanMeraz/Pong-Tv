36ca19d3b73f8bf1851ece1dcad49bce45071ba7
Juan Meraz <14meraz98@mail.com>
2020-03-12 19:07:09 -0700

    first commit

---
+++ Ball.js
@@ -0,0 +1,29 @@
+class Ball{
+
+	constructor(x,y,d,c){
+		this.x = x
+		this.y = y
+		this.d = d
+		this.c = c
+		this.speedX= 2 * this.direction()
+		this.speedY= 2 * this.direction()
+	}
+
+	draw(){
+		push()
+		noStroke()
+		fill(this.c)
+		ellipse(this.x,this.y,this.d,this.d)
+		pop()
+	}
+
+	move(){
+		this.x+= this.speedX
+		this.y+= this.speedY
+	}
+
+	direction(){
+		return floor(ramdom(2)*2-1
+	}
+	
+}
